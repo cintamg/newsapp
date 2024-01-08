a7b3943cf09a1d3c952fa229797436b3f84b07d8
cintamg <76482225+cintamg@users.noreply.github.com>
2024-01-07 23:13:29 -0800

    previous and next btns

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -9,7 +9,7 @@ export default class NewsItem extends Component {
         <img src={imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}...</p>
+                <p className="card-text">{description}</p>
                 <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
